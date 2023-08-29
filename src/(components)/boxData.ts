1e7b38e56cf60f84169156ac227081a34bb84706
Mozibix <opuzmoses@gmail.com>
2023-08-29 21:08:07 +0100

    chores: updated boxData component

---
+++ src/(components)/boxData.ts
@@ -0,0 +1,39 @@
+const BoxData = [
+  {
+    id: 1,
+    icon_src: "/svgs/icon_1.svg",
+    header_text: "MakeAI-assisted Content Choices",
+    box_quotes:
+      "Access and Order from an extensive catalogue in any language and on your budget's terms. If unsure, our AI guides you to the best.",
+  },
+  {
+    id: 2,
+    icon_src: "/svgs/icon_2.svg",
+    header_text: "Upload and Maintain your Brand Identity",
+    box_quotes:
+      "Add your preferences and brand assets to ensure the contents is consistent with your brand identity. No brand asset? We can make for you!",
+  },
+  {
+    id: 3,
+    icon_src: "/svgs/icon_3.svg",
+    header_text: "Be informed as we create",
+    box_quotes:
+      "Your details are translated and sent to the AI, FP, or both (per your choice) as you relax and receive continuous updates.",
+  },
+  {
+    id: 4,
+    icon_src: "/svgs/icon_4.svg",
+    header_text: "Receive and Review",
+    box_quotes:
+      "The finished content is delivered and you can make corrections that we will effect to satisfy your requirements.",
+  },
+  {
+    id: 5,
+    icon_src: "/svgs/icon_5.svg",
+    header_text: "Publish and Monitor your contents' progress",
+    box_quotes:
+      "The finished content is delivered and you can make corrections that we will effect to satisfy your requirements.",
+  },
+];
+
+export default BoxData;
