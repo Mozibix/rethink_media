330c358cf1a733b66b43c97f31e158720f99e232
Mozibix <opuzmoses@gmail.com>
2023-08-31 02:29:02 +0100

    feat: updated scroll animation feature

--- src/(components)/Header.tsx
+++ src/(components)/Header.tsx
@@ -3,8 +3,13 @@ import logo from "/svgs/logo.svg";
 const Header = () => {
   return (
     <>
-      <header className="header">
-        <div className="header_inner container_main">
+      <header className="header" id="header">
+        <div
+          className="header_inner container_main"
+          data-aos="flip-right"
+          data-aos-delay="100"
+          data-aos-duration="1000"
+        >
           <img src={logo} alt="rethink-logo" className="logo" />
         </div>
       </header>
